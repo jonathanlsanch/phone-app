65636e4d25dcb65aa7ff5369e4c8d2e333f259aa
Jonathan Sanchez <jonathansanchez@Jonathans-MacBook-Pro.local>
2018-03-09 09:30:43 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PhoneService } from './phone.service';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { PhoneListComponent } from './phone-list/phone-list.component';
+import { PhoneDetailsComponent } from './phone-details/phone-details.component';
+
+import { RouterModule } from "@angular/router";
+import { routes } from './app.routing';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneListComponent,
+    PhoneDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [PhoneService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/phone-details/phone-details.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { PhoneService } from './../phone.service';
+
+@Component({
+  selector: 'app-phone-details',
+  templateUrl: './phone-details.component.html',
+  styleUrls: ['./phone-details.component.css'],
+  providers: [PhoneService]
+})
+export class PhoneDetailsComponent implements OnInit {
+  phone: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private phoneService: PhoneService) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.getPhoneDetails(params['id']);
+    });
+  }
+
+  getPhoneDetails(id) {
+    this.phoneService.get(id)
+      .subscribe((phone) => {
+        this.phone = phone;
+      });
+  }
+
+    deletePhone() {
+      if (window.confirm('Are you sure?')) {
+        this.phoneService.remove(this.phone._id)
+          .subscribe(() => {
+            this.router.navigate(['']);
+          });
+      }
+  }
+}
